4fd87e383a3c91357a2217d8a7daf0c6c0b2369f
teddyrugged <jakpaetuk@gmail.com>
2022-09-21 22:22:11 +0100

    new

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,16 +1,21 @@
-import { map } from "fontawesome";
+// import { map } from "fontawesome";
 import React, { useState, useEffect } from "react";
 import { getMovies } from "../services/fakeMovieService";
 
 const Movies = () => {
     const allMovies = getMovies();
     const [movies, setMovies] = useState([]);
+    // const [handleDelete, setHandleDelete] = useState('');
+    // handleDelete = movie => { console.log(movie)};
+    const handleDelete = (id) => {
+      setMovies(movies.filter(deck => deck.id !== id));
+   }
 
   useEffect(() => {
     setMovies(allMovies)
 
 
-  }, []);
+  }, [allMovies]);
   console.log(movies)
 
   return (
@@ -21,6 +26,7 @@ const Movies = () => {
           <th>Genre</th>
           <th>Stock</th>
           <th>Rate</th>
+          <th></th>
         </tr>
       </thead>
       <tbody>
@@ -30,6 +36,7 @@ const Movies = () => {
                 <td> { movie.genre.name}</td>
                 <td> { movie.numberInStock}</td>
                 <td> { movie.dailyRentalRate}</td>
+                <td><button onClick={() => handleDelete= { handleDelete}} className="btn btn-danger btn-sm">Delete</button></td>
             </tr>
 
         ))}
@@ -41,3 +48,6 @@ const Movies = () => {
 };
 
 export default Movies;
+
+
+// how to get the stock data of Tesla in python?
