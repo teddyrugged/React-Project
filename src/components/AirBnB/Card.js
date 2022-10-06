4e487eb103f18e3d6ffa5c06a8845a1b90136df9
teddyrugged <jakpaetuk@gmail.com>
2022-10-06 16:16:29 +0100

    fixed

--- src/components/AirBnB/Card.js
+++ src/components/AirBnB/Card.js
@@ -1,46 +1,52 @@
 import React from 'react';
 import './Navbar.css';
-import man from './images/image 12.svg';
 import star from './images/Star 1.svg';
-import wedding from './images/wedding-photography 1.svg';
-import bike from './images/mountain-bike 1.svg';
+// import wedding from './images/wedding-photography 1.svg';
+// import bike from './images/mountain-bike 1.svg';
 
-const Card = () => {
+const Card = (props) => {
   return (
     <div className='card--grid'>
         <div className='card--one'>
-            <img src={ man } alt = "a pic" className="card--man" />
-            <div>
+            <img src={props.img} alt = "a pic" className="card--man" />
+            {/* <img src={require('./images/' + props.img)} alt = "a pic" className="card--man" /> */}
+            <div className="card--set">
                 <img src={ star} alt = "a pic" className="card--star" />
-                <span>5.0 </span>
-                <span>(6).</span>
-                <span>USA</span>
+                <span>{ props.rating } </span>
+                <span className="gray">({props.reviewCount}) {""}.</span>
+                <span className="gray">{props.location}</span>
             </div>
-            <p>Life lessons with Katie Zaferes</p>
-            <p>From $136 / person</p>
+            <p>{props.title}</p>
+            <p><span className='tag--bold'>From ${ props.price}</span>/ person</p>
         </div>
-        <div className='card--two'>
-            <img src={ wedding } alt = "a pic" className="card-wedding" />
+        {/* <div className='card--two'>
+            <img src={ wedding } alt = "a pic" className="card--wedding" />
             <div>
-                <img src={ star} alt = "a pic" className="card--star" />
-                <span>5.0 </span>
-                <span>(30).</span>
-                <span>USA</span>
+                <div className="card--set">
+                    <img src={ star} alt = "a pic" className="card--star" />
+                    <span>5.0 </span>
+                    <span className="gray">(30).</span>
+                    <span className="gray">USA</span>
+                </div>
+
             </div>
             <p>Learn wedding photography</p>
-            <p>From $125 / person</p>
-        </div>
-        <div className='card--three'>
+            <p><span className='tag--bold'>From $125 </span>/ person</p>
+        </div> */}
+        {/* <div className='card--three'>
             <img src={ bike } alt = "a pic" className="card--bike" />
             <div>
-                <img src={ star} alt = "a pic" className="card--star" />
-                <span>4.8 </span>
-                <span>(2).</span>
-                <span>USA</span>
+
+                <div className="card--set">
+                    <img src={ star} alt = "a pic" className="card--star" />
+                    <span>4.8 </span>
+                    <span className="gray">(2).</span>
+                    <span className="gray">USA</span>
+                </div>
             </div>
             <p>Group Mountain Biking</p>
-            <p>From $50 / person</p>
-        </div>
+            <p><span className='tag--bold'>From $50 </span>/ person</p>
+        </div> */}
 
     </div>
 
@@ -48,3 +54,24 @@ const Card = () => {
 }
 
 export default Card
+
+
+
+
+
+
+// eslint-disable-next-line
+{/* <div className='card--one'>
+            <img src={`../AirBnB/images/  ${ props.img}` } alt = "a pic" className="card--man" /> */}
+            // eslint-disable-next-line
+            {/* <img src={require('./images/' + props.img)} alt = "a pic" className="card--man" /> */}
+            // eslint-disable-next-line
+            {/* <div className="card--set">
+                <img src={ star} alt = "a pic" className="card--star" />
+                <span>{ props.rating } </span>
+                <span className="gray">({props.reviewCount}) {""}.</span>
+                <span className="gray">{props.location}</span>
+            </div>
+            <p>{props.title}</p>
+            <p><span className='tag--bold'>From ${ props.price}</span>/ person</p>
+</div> */}
--- src/pages/AirBnB/AirBnB.js
+++ src/pages/AirBnB/AirBnB.js
@@ -2,17 +2,36 @@ import React from 'react';
 import Navbar from '../../components/AirBnB/Navbar';
 import HeroSection  from '../../components/AirBnB/HeroSection';
 import Card from '../../components/AirBnB/Card';
+import data from "../../components/AirBnB/data";
 
 
 const AirBnB = () => {
+
+  const datas = data.map( item => {
+    return (
+      <Card
+        img={item.coverImg}
+        rating={item.stats.rating}
+        reviewCount={item.stats.reviewCount}
+        location={item.location}
+        title={item.title}
+        price={item.price}
+        />
+        )
+    })
+
+
   return (
     <div>
 
       < Navbar/>
       <HeroSection />
-      <Card />
+      {datas}
+
     </div>
   )
 }
 
 export default AirBnB
+
+
